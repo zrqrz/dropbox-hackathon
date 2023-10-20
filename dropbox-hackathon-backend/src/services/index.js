7ab8a95a657a0eb946d1b77df7cdf8bb1e1f6675
zrqrz <76686565+zrqrz@users.noreply.github.com>
2023-10-19 19:20:10 -0700

    Removed the original subdirectory

--- dropbox-hackathon-backend/src/services/index.js
+++
@@ -1,9 +0,0 @@
-module.exports.authService = require('./auth.service');
-module.exports.emailService = require('./email.service');
-module.exports.tokenService = require('./token.service');
-module.exports.userService = require('./user.service');
-module.exports.dropboxService = require('./dropbox.service');
-module.exports.petitionService = require('./petition.service');
-module.exports.openaiService = require('./openai.service');
-module.exports.convertService = require('./convert.service');
-
